f874f4a3c80528ccb6519e0d7f10e51e7d63dc46
Smita3434 <sayalig94@gmail.com>
2022-07-20 20:15:40 +0530

    Add project files.

---
+++ ADO.netUsingDapper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADO.netUsingDapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
